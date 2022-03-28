52b71985dc53be37c87651b92a6c18158fc6af6a
Luna <lunacodes@gmail.com>
2022-03-28 17:37:22 -0400

    Add bootstrap as node module, and basic styles. Update Nav menu

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,5 @@
 import React from "react";
-
-// We import bootstrap to make our application look better.
-import "bootstrap/dist/css/bootstrap.css";
+// import 'bootstrap/dist/js/bootstrap.min.js';
 
 // We import NavLink to utilize the react router.
 import { NavLink } from "react-router-dom";
@@ -9,33 +7,20 @@ import { NavLink } from "react-router-dom";
 // Here, we display our Navbar
 export default function Navbar() {
  return (
-	 <div>
-		 <nav className="navbar navbar-expand-lg navbar-light bg-light">
-			 <NavLink className="navbar-brand" to="/">
-			 <img style={{"width" : 25 + '%'}} src="https://d3cy9zhslanhfa.cloudfront.net/media/3800C044-6298-4575-A05D5C6B7623EE37/4B45D0EC-3482-4759-82DA37D8EA07D229/webimage-8A27671A-8A53-45DC-89D7BF8537F15A0D.png" alt="MongoDB logo"></img>
-			 </NavLink>
-			 <button
-				 className="navbar-toggler"
-				 type="button"
-				 data-toggle="collapse"
-				 data-target="#navbarSupportedContent"
-				 aria-controls="navbarSupportedContent"
-				 aria-expanded="false"
-				 aria-label="Toggle navigation"
-			 >
-				 <span className="navbar-toggler-icon"></span>
-			 </button>
-
-			 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-				 <ul className="navbar-nav ml-auto">
-					 <li className="nav-item">
-						 <NavLink className="nav-link" to="/add">
-							 Add Webtoon
-						 </NavLink>
-					 </li>
-				 </ul>
-			 </div>
-		 </nav>
-	 </div>
+ 	<nav className="container navbar navbar-expand-lg navbar-light top-nav sticky-top" aria-label="main navigation">
+ 	  <div className="nav-wrap container-fluid">
+	 	  <NavLink className="navbar-brand" to="/">MyWebtoonList</NavLink>
+ 	    <button className="navbar-toggler navbar-toggler-right ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#topNav" aria-controls="topNav" aria-expanded="false" aria-label="Toggle navigation">
+ 	      <span className="navbar-toggler-icon"></span>
+ 	    </button>
+ 	    <div className="collapse navbar-collapse" id="topNav">
+ 	      <ul className="nav navbar-nav ms-auto mt-2 mt-lg-0">
+ 	        <li className="nav-item">
+	 	        <NavLink className="nav-link" to="/profile">Profile</NavLink>
+ 	        </li>
+ 	      </ul>
+ 	    </div>
+ 	  </div>
+ 	</nav>
  );
 }
