4c7bec148317e841e5ab8e93565dad178d311cf9
Luna <lunacodes@gmail.com>
2022-04-13 16:08:37 -0400

    Re-add server models

---
+++ server/models/User.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
+
+// const User = new Schema({
+// 	name_first: String,
+// 	name_last: String,
+// 	username: String,
+// 	pass: String,
+// });
+
+const Session = new Schema({
+	refreshToken: {
+		type: String,
+		default: '',
+	},
+});
+
+const User = new Schema({
+	firstName: {
+		type: String,
+		default: '',
+	},
+	lastName: {
+		type: String,
+		default: '',
+	},
+	authStrategy: {
+		type: String,
+		default: 'local',
+	},
+	points: {
+		type: Number,
+		default: 50,
+	},
+	refreshToken: {
+		type: [Session],
+	},
+});
+
+//Remove refreshToken from the response
+User.set('toJSON', {
+	transform: function (doc, ret, options) {
+		delete ret.refreshToken;
+		return ret;
+	},
+});
+
+User.plugin(passportLocalMongoose);
+module.exports = mongoose.model('users', User);
---
+++ server/models/Webtoon.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const webtoonSchema = new Schema({
+	title: String,
+	score: String,
+	progress: String,
+	tags: String,
+});
+
+module.exports = mongoose.model('webtoons', webtoonSchema);
