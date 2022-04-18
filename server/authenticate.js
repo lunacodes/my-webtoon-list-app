edc2ba23104480acac55bab41f0b3b53b3507e10
Luna <lunacodes@gmail.com>
2022-04-18 17:31:55 -0400

    Token Fetch debug

--- server/authenticate.js
+++ server/authenticate.js
@@ -4,13 +4,14 @@ const dev = process.env.NODE_ENV !== 'production';
 
 exports.COOKIE_OPTIONS = {
 	'Access-Control-Allow-Origin': '*',
+	'Access-Control-Allow-Headers': 'Accept',
 	httpOnly: true,
 	// Since localhost is not having https protocol,
 	// secure cookies do not work correctly (in postman)
-	secure: !dev,
+	secure: true,
 	signed: true,
 	maxAge: eval(process.env.REFRESH_TOKEN_EXPIRY) * 1000,
-	sameSite: 'Lax',
+	sameSite: 'none',
 };
 
 exports.getToken = (user) => {
